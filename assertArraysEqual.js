c07fb473447ef9db4d629af79e3fb1b5d520d9d8
carmen kuang <carmenkuang87@gmail.com>
2022-02-09 04:37:21 +0000

    assertArraysEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,20 @@
+const assertArraysEqual = function(arr1, arr2) {
+  const eqArrays = function(arr1, arr2) {
+    if (arr1.length !== arr2.length) return false;
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) return false;
+    }
+    return true;
+  };
+
+  if (eqArrays(arr1, arr2)) {
+    console.log(`😁😁😁Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`😭😭😭Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+
+assertArraysEqual([1,2,3],[1,2,3]);
+assertArraysEqual([1,2,3],[1,'2',3]);
+
