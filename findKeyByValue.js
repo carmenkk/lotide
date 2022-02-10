e22ac19dfcc4897035b3a8786b185ae2842a270d
carmen kuang <carmenkuang87@gmail.com>
2022-02-10 03:34:44 +0000

    object.keys

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😁😁😁Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`😭😭😭Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const findKeyByValue = function(input,item) {
+  let arr = Object.keys(input);
+  for (const category of arr) {
+    if (input[category] === item) {
+      return category;
+    }
+  }
+};
+    
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
