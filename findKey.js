191032aaff8933c0040a32cc645e7009d26a6f92
carmen kuang <carmenkuang87@gmail.com>
2022-02-11 04:30:08 +0000

    findKey

---
+++ findKey.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😁😁😁Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`😭😭😭Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const findKey = function(input, callback) {
+  for (let element in input) {
+    if (callback(input[element]) === true) {
+      return element;
+      
+    }
+  }
+  
+};
+
+
+ const a =findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+assertEqual(a,"noma");
