a0ea2fd0e436fd355040567795c92269a631f5f4
carmen kuang <carmenkuang87@gmail.com>
2022-02-14 20:47:03 +0000

    refactor eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`😁😁😁Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`😭😭😭Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+
 
 const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) return false;
@@ -16,4 +10,4 @@ const eqArrays = function(arr1, arr2) {
 
 
  
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+module.exports = eqArrays;
