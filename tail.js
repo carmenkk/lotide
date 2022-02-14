d73d730a60e571d1d689ab8529a4789fa89f5ab9
carmen kuang <carmenkuang87@gmail.com>
2022-02-14 20:40:58 +0000

    refactor tail

--- tail.js
+++ tail.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`😁😁😁Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`😭😭😭Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+
 
 const tail = arr => {
   let newArr = arr.slice(1);
@@ -12,7 +6,4 @@ const tail = arr => {
 };
 
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs");
+module.exports = tail;
