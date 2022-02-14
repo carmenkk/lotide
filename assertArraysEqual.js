005dac1f3dc0a822882f39eb1fbd566cdc0e936e
carmen kuang <carmenkuang87@gmail.com>
2022-02-14 21:08:08 +0000

    refactor assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,12 +1,7 @@
-const assertArraysEqual = function(arr1, arr2) {
-  const eqArrays = function(arr1, arr2) {
-    if (arr1.length !== arr2.length) return false;
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) return false;
-    }
-    return true;
-  };
+const eqArrays = require('./eqArrays');
 
+const assertArraysEqual = function(arr1, arr2) {
+  
   if (eqArrays(arr1, arr2)) {
     console.log(`😁😁😁Assertion Passed: ${arr1} === ${arr2}`);
   } else {
@@ -15,6 +10,6 @@ const assertArraysEqual = function(arr1, arr2) {
 };
 
 
-assertArraysEqual([1,2,3],[1,2,3]);
-assertArraysEqual([1,2,3],[1,'2',3]);
 
+
+module.exports = assertArraysEqual;
